3e9b20a631287be7f2a01f211078b63d5aba165b
Nishant Patel <contact@nishant-patel.com>
2017-03-14 20:29:52 -0700

    Rainbow

--- Rainbow-Road/ViewController.swift
+++ Rainbow-Road/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let cellsize = 6
+    let cellColor = [UIColor.red,UIColor.orange,UIColor.yellow, UIColor.green,UIColor.blue, UIColor.purple]
 
+    @IBOutlet weak var tableView: UITableView!    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.rowHeight = 120;
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellsize
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        cell.contentView.backgroundColor = cellColor[indexPath.row]            
+        return cell
+    }
+    
+}
